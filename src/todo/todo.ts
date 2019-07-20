32079fd7370162632350e8502918b13a5585311b
Will Ockelmann-Wagner <will.ockelmann.wagner@gmail.com>
2019-07-19 19:05:00 -0700

    wip create and date

--- src/todo/todo.ts
+++ src/todo/todo.ts
@@ -1,11 +1,18 @@
 interface Todo {
-  id: string | number;
+  id: string | number | null;
   title: string;
   done: boolean;
-  createdAt: string;
+  createdAt: string | null;
 }
 
 type TodoList = Todo[];
 
 export type T = Todo;
 export type List = TodoList;
+
+export const EMPTY: Todo = {
+  id: null,
+  title: "",
+  done: false,
+  createdAt: null
+};
